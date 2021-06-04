baec808ae4313ca689f646a69a72ffea2d054b26
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-06-05 00:42:45 +0200

    add the cors usage to the comments service

--- comments/index.js
+++ comments/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
-//const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 //app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 const commentsByPostId = {};
 app.get("/posts/:id/comments", (req, res) => {
