8adf0195f2ac416602844ac7e61ffb56ad5da447
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-06-05 23:47:26 +0200

    add the logic to emit event of comment created

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 //app.use(express.urlencoded({ extended: true }));
@@ -12,7 +13,7 @@ app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { id } = req.params;
     const { content } = req.body;
@@ -20,6 +21,17 @@ app.post("/posts/:id/comments", (req, res) => {
     const comments = commentsByPostId[id] || [];
     comments.push({ id: commentId, content });
     commentsByPostId[id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: id
+        }
+
+    });
+
     res.status(201).send(comments);
 });
 
