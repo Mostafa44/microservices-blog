d62d962260820425331d6e1902fb5e10f3e597ea
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-06-06 02:24:55 +0200

    correct the missed typed url for the post calls

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     const event = req.body;
 
-    axios.post('localhost:4000/events', event);
-    axios.post('localhost:4002/events', event);
-    axios.post('localhost:4003/events', event);
+    await axios.post('http://localhost:4000/events', event);
+    await axios.post('http://localhost:4002/events', event);
+    await axios.post('http://localhost:4003/events', event);
 
     console.log('all posts have been made');
 
