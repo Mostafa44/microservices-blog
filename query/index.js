aef1e003b014c8688ca4e66e75e64626ce303e6b
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-06-06 03:36:18 +0200

    add the logic for the query service

--- query/index.js
+++ query/index.js
@@ -5,12 +5,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const posts = {};
+
 app.post('/events', (req, res) => {
 
+    const { type, data } = req.body;
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    }
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+    console.log(posts);
+    res.send({});
 });
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts);
 });
 
 app.listen(4003, () => {
