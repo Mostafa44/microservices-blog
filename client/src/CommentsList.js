c6a52c1c5e0d65214319662448302846d5ca7af5
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-06-07 05:39:05 +0200

    update the logic in react application to be using the query service

--- client/src/CommentsList.js
+++ client/src/CommentsList.js
@@ -1,15 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const CommentsList = ({ postId }) => {
-    const [comments, setComments] = useState([]);
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4002/posts/${postId}/comments`);
-        setComments(res.data);
-    }
-    useEffect(() => {
-        fetchComments();
-    }, []);
+
+const CommentsList = ({ comments }) => {
+
 
     const renderedComments = comments.map(comment => {
         return (
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,8 @@ const PostList = () => {
 
     const [posts, setPosts] = useState({});
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4003/posts');
+        //console.log(res.data);
         setPosts(res.data);
     }
     useEffect(() => {
@@ -20,7 +21,7 @@ const PostList = () => {
             style={{ width: '30%', marginBottom: '20px' }}>
             <div className="card-body">
                 <h3>{post.title}</h3>
-                <CommentsList postId={post.id} />
+                <CommentsList comments={post.comments} />
                 <CommentCreate postId={post.id} />
             </div>
         </div>);
