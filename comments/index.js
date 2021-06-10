29160da011ea99f3a3afe0dcee546025aa629acf
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-06-10 06:56:30 +0200

    add the logic to handle the comment moderated event in the comments service

--- comments/index.js
+++ comments/index.js
@@ -36,9 +36,28 @@ app.post("/posts/:id/comments", async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     const event = req.body;
     console.log(' Event Recieved ', event.type);
+    const { type, data } = event;
+    if (type === 'CommentModerated') {
+        const { id, content, postId, status } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
+            }
+        })
+    }
     res.send({});
 });
 app.listen(4002, () => {
