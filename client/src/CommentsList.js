9ae6a40b31d98664b7cf92292a6be5b854a2602c
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-06-10 22:15:51 +0200

    rendering comments by status

--- client/src/CommentsList.js
+++ client/src/CommentsList.js
@@ -3,11 +3,20 @@ import React from 'react';
 
 const CommentsList = ({ comments }) => {
 
-
+    let content;
     const renderedComments = comments.map(comment => {
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+        if (comment.status === 'pending') {
+            content = 'this comment is waiting for moderation';
+        }
+        if (comment.status === 'rejected') {
+            content = 'this comment was rejected';
+        }
         return (
             <li key={comment.id}>
-                {comment.content}
+                {content}
             </li>
         )
 
